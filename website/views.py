f74cad5e3704bded7f5f1231d7e0fa825d4bfbcd
Hossein <139358833+HosseinnFazlii@users.noreply.github.com>
2023-11-05 08:16:57 +0330

    load statics

--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def index_view(requesta):
-    return HttpResponse('HELLO')
+def index_view(request):
+    return render(request,'website/index.html' )
