2addff47930c23c52e6481b40a6a76a9a1fcd6e2
Hossein <139358833+HosseinnFazlii@users.noreply.github.com>
2023-11-14 01:13:26 +0330

    ok

--- blog/models.py
+++ blog/models.py
@@ -4,4 +4,12 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    created_date= models.DateTimeField(auto_now_add=True)
+    updated_date=models.DateTimeField(auto_now=True)
+    publish_date=models.DateTimeField(null=True)
+    status=models.BooleanField(default=False)
+    counted_view=models.IntegerField(default=0)
     
+
+    def __str__(self):
+        return "{}-{}-{}".format(self.title,self.status,self.id)
