c063fff781fdcd13c8dc54d881fdef9b8cd6c120
Hossein <139358833+HosseinnFazlii@users.noreply.github.com>
2023-11-27 17:30:26 +0330

    commit

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,7 @@ class Post(models.Model):
     publish_date=models.DateTimeField(null=True)
     status=models.BooleanField(default=False)
     counted_view=models.IntegerField(default=0)
-    image=models.ImageField(upload_to='blog/')
+    image=models.ImageField(upload_to='blog/',default='blog/default.jpg')
     
 
     def __str__(self):
