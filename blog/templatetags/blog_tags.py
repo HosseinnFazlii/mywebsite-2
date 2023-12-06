f715be9a15617b2607018bfb0308653ce421ae30
Hossein <139358833+HosseinnFazlii@users.noreply.github.com>
2023-12-06 09:19:15 +0330

    inclusion tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,4 +1,14 @@
 from django import template
+from blog.models import Post
 
 register= template.Library()
 
+@register.filter
+def snippet(value):
+    return value[:100]
+
+
+@register.inclusion_tag('blog/latestposts.html')
+def latestposts():
+    posts=Post.objects.filter(status=1).order_by('publish_date')[:3 ]
+    return {'posts':posts}
